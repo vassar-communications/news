e5a80400f4520211ba52dace0288a571b92605c8
Jeff Macaluso <sj.macaluso.biz@gmail.com>
2018-07-12 16:54:42 -0400

    intial commit
    config, sass, htaccess, 404, gitignore

---
+++ assets/config.php
@@ -0,0 +1,24 @@
+<?php
+//error_reporting(E_ALL);
+return 
+array(
+    'phpErrorReporting' => E_ALL,
+    'siteID' => '26465',
+    'siteDesignType' => 'academic',
+    'vassarCollegeAppend' => '',
+    // 'subSiteNavigationAdditional' => array('grandparent','All Exhibits'),
+    'headingHierarchy' => true,
+    'typekitID' => 'mvb3wrl',
+    'siteNameHeader' => "News and Events",
+    // 'siteNameSubhead' => "All the news that's fit to not print.",
+    'vassarCollegeIDHome' => $vassarCollegeIDHome,
+    'vassarCollegeID' => $vassarCollegeIDHome,
+    'searchNew' => true,
+    // 'useRootNav' => true,
+    'globalNav' => true,
+    'siteNameFooter' => 'Vassar College',
+    'officeLocationBuilding' => "",
+    'mastheadLink' => false,
+    'newsKeywords' => 'Info',
+    'globalFooter' => true,
+);
